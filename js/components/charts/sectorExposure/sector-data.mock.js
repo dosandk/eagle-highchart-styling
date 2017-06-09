6e2dd5ab88b4ab8c2c6acbb872d8f8011cb8bb39
Volodymyr Shevchuk <Volodymyr.Shevchuk@eleks.com>
2017-06-09 13:05:53 +0300

    Changed data;

--- js/components/charts/sectorExposure/sector-data.mock.js
+++ js/components/charts/sectorExposure/sector-data.mock.js
@@ -1,41 +1,41 @@
 export const mockedData = [
   {
     'y': 0.163168,
-    'name': 'Industrials',
-    'description': 'Industrials',
-    'marketValue': 1784948387.39,
+    'name': 'accusamus',
+    'description': 'minus',
+    'marketValue': 82742,
     'sectorCode': '20',
     'color': 'rgb(96,170,100)'
   },
   {
     'y': 0.156078,
-    'name': 'Fixed Income Securities',
-    'description': 'Fixed Income Securities',
-    'marketValue': 1707360656.1399999,
+    'name': 'vero',
+    'description': 'in',
+    'marketValue': 67449,
     'sectorCode': '60',
     'color': 'rgb(108,182,112)'
   },
   {
     'y': 0.122943,
-    'name': 'Energy',
-    'description': 'Energy',
-    'marketValue': 1344846759.25,
+    'name': 'dolorum',
+    'description': 'impedit',
+    'marketValue': 72535,
     'sectorCode': '10',
     'color': 'rgb(121,195,125)'
   },
   {
     'y': 0.09103299999999999,
-    'name': 'Consumer Discretionary',
-    'description': 'Consumer Discretionary',
-    'marketValue': 995870141.75,
+    'name': 'illum',
+    'description': 'nemo',
+    'marketValue': 95604,
     'sectorCode': '25',
     'color': 'rgb(134,208,138)'
   },
   {
     'y': 0.46672099999999994,
-    'name': 'Other Investment Products',
-    'description': 'Other Investment Products',
-    'marketValue': 5105552476.55,
+    'name': 'suscipit',
+    'description': 'quia',
+    'marketValue': 16643,
     'sectorCode': 'others sector code',
     'color': 'rgb(147,221,151)'
   }
